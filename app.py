a3f821b64aabe4dd49b7a14eb8c7f144679ce394
GeorginaMoraa <georginagee02@gmail.com>
2025-03-25 18:16:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
-from flask_socketio import SocketIO
 from config import Config
 from utils.database import init_db
 from routes.auth_routes import auth_bp
@@ -15,7 +14,6 @@ app.config.from_object(Config)
 # Initialize Extensions
 init_db(app)
 jwt = JWTManager(app)
-socketio = SocketIO(app)
 
 # Register Blueprints
 app.register_blueprint(auth_bp, url_prefix='/api/auth')
@@ -24,19 +22,5 @@ app.register_blueprint(report_bp, url_prefix='/api/report')
 app.register_blueprint(device_bp, url_prefix='/api/device')
 app.register_blueprint(room_bp, url_prefix="/api/room")
 
-# WebSocket Events
-# @socketio.on('connect')
-# def handle_connect():
-#     print("Client connected")
-
-# @socketio.on('disconnect')
-# def handle_disconnect():
-#     print("Client disconnected")
-
-# @socketio.on('alert')
-# def handle_alert(data):
-#     print(f"Alert received: {data}")
-#     socketio.emit('notification', {"msg": "Energy alert!"})
-
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True)
