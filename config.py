54231165c04f6089758f516b17d0e9ba58f81d3c
GeorginaMoraa <georginagee02@gmail.com>
2025-03-26 08:35:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://bridge:oWOpozyihRVNBIFP@cluster0.xk1esci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://bridge:oWOpozyihRVNBIFP@cluster0.xk1esci.mongodb.net/power_savvy?retryWrites=true&w=majority&appName=Cluster0")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your_jwt_secret")
